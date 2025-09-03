47a36c5b10ff1a59db3a84d2f96166d97c976281
jay b <jbarksdale2000@gmail.com>
2025-09-02 22:01:52 -0400

    Update page.tsx

--- app/public/signin/page.tsx
+++ app/public/signin/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from 'react';
-import { createClient } from '@/lib/supabase';
+import { createClient } from '../../../lib/supabase';
 
 export default function SignInPage() {
   const [email, setEmail] = useState('');
