3d7e0aa91c3796ce2755f3bc4757487dea80deed
jay b <jbarksdale2000@gmail.com>
2025-09-04 20:07:59 -0400

    Update supabase-client.ts

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,5 +1,5 @@
 import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
-import { createClient } from '../../lib/supabase-client';
+import { createClient } from '../../../lib/supabase-client';
 export const createClient = () =>
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL || '',
